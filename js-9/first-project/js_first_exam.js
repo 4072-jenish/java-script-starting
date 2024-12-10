7618db3efcf2d2e072de7f7213a104da556662f0
hariyanijenish@gmail.com <malaviyapunam3@gmail.com>
2024-12-10 09:51:47 +0530

    javascript learning

---
+++ js-9/first-project/js_first_exam.js
@@ -0,0 +1,222 @@
+{
+  // Q1 WAP to find area of triangale :
+
+console.log("findout area of triangale: ");
+
+
+let base=8, height =10 , area;
+
+  area= 1/2*base*height;
+
+
+console.log("Base : ", base);
+console.log("Height : ", height);
+console.log("area of triangale : ", area);
+
+// base = 8 
+// height = 10 
+
+// output
+
+// area of triangale is = 40.
+}
+
+{
+  // Q2 WAP to convert tempretuers to and from Celsius , ferenhit : 
+
+console.log("converts celsius into ferenhit ");
+
+
+let celsius = 20, ferenhit;
+
+ferenhit =  (celsius* 9/5)+32;
+
+
+console.log("celsius : ", celsius);
+console.log("ferenhit : ", ferenhit);
+
+// celsius = 20
+
+// output
+
+// celsius convert to ferenhit is  = 68.
+}
+
+{
+  // Q3 WAP to find the area of rectangale :
+
+console.log("findout area of rectangale: ");
+
+let width=8, haight =10 , area;
+
+  area= width*haight;
+
+
+console.log("Width : ", width);
+console.log("Height : ", haight);
+console.log("area of rectangale : ", area);
+
+// width = 8 
+// height = 10 
+
+// output
+
+// area of rectangale is = 80.
+}
+
+{
+  //  Q4 WAP to find area of circle :
+
+console.log("findout area of circle: ");
+
+
+let r=8, area;
+const pai=3.14;
+
+  area = pai* r* r;
+
+
+console.log("redius : ", r);
+console.log("area of circle : ", area);
+
+
+// redius = 8 
+// pai = 3.14
+
+// output
+
+// area of circle is =  201.14
+}
+
+{
+  // Q5 WAP to convert feets into inches :
+
+console.log("converts feet into iches ");
+
+
+let feet = 18, inches;
+
+inches = feet *12;
+
+
+console.log("feets : ", feet);
+console.log("Inches : ", inches);
+
+// feet = 18
+
+// output
+
+// feets converts into inches is  =  216
+}
+
+{
+  // Q6 calculate given formulas
+  console.log("Q6 calculate given formulas");
+   
+
+  {
+    // Q1 a*a - b*b = (a-b)(a+b) ;
+
+    console.log("Q1 a*a - b*b = (a-b)(a+b)");
+    
+      let a = 5, b = 7, c ;
+      c =   (a-b)*(a+b);
+      
+      console.log("((a*a)-(b*b))  = ", c);
+      
+      // a = 5
+      // b = 7
+
+      // output
+
+      // c = -24
+
+  }
+  {
+    // Q2 ((a-b)*(a-b)) = a*a - 2*a*b + b*b ;
+
+    console.log("Q2 ((a-b)*(a-b)) = a*a - 2*a*b + b*b");
+    
+
+        let a = 5, b = 7, c ;
+         c =   a*a - 2*a*b + b*b;
+      
+      console.log(" ((a-b)*(a-b)) = ", c);
+      
+      // a = 5
+      // b = 7
+
+      // output
+
+      // ((a-b)*(a-b)) = 4
+
+  }
+   
+  {
+     // Q3 ((a+b+c)*(a+b+c)) = a*a + b*b + c*c + 2*a*b+ 2*b*c + 2*c*a ;
+
+     console.log("Q3 ((a+b+c)*(a+b+c)) = a*a + b*b + c*c + 2*a*b+ 2*b*c + 2*c*a");
+    
+
+     let a = 5, b = 7, c = 9, e;
+   e =   a*a + b*b + c*c + 2*a*b+ 2*b*c + 2*c*a;
+   
+   console.log(" ((a+b+c)*(a+b+c)) ", e);
+   
+   // a = 5
+   // b = 7
+   // c = 9
+
+   // output
+
+   // ((a+b+c)*(a+b+c)) = 441
+
+   }
+  {
+     // Q4 ((a-b-c)*(a-b-c)) = a*a + b*b + c*c - 2*a*b - 2*b*c + 2*c*a ;
+
+     console.log("Q4 ((a-b-c)*(a-b-c)) = a*a + b*b + c*c - 2*a*b - 2*b*c + 2*c*a ");
+    
+
+     let a = 5, b = 7, c = 9, e;
+   e =   a*a + b*b + c*c - 2*a*b - 2*b*c + 2*c*a;
+   
+   console.log(" ((a-b-c)*(a-b-c)) ", e);
+   
+   // a = 5
+   // b = 7
+   // c = 9
+
+   // output
+
+   // ((a-b-c)*(a-b-c)) = 49
+
+   }
+   {
+  // Q5 ((a-b)*(a-b)*(a-b)) = a*a*a - 3*(a*a)*b + 3*a*(b*b)- b*b*b ;
+
+     console.log("Q4 ((a-b)*(a-b)*(a-b)) = a*a*a - 3*(a*a)*b + 3*a*(b*b)- b*b*b");
+    
+
+      let a = 5, b = 7, c ;
+      c = a*a*a - 3*(a*a)*b + 3*a*(b*b)- b*b*b;
+   
+   console.log(" ((a-b)*(a-b)*(a-b)) ", c);
+   
+   // a = 5
+   // b = 7
+   // c = 9
+
+   // output
+
+   // ((a-b)*(a-b)*(a-b)) = -8
+
+   }
+
+
+
+
+
+}
+
+
---
+++ js-9/lc-2-js/operator.js
@@ -0,0 +1,1 @@
+document.getElementById("fuka").value;
