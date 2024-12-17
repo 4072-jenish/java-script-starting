a7a79da782cf4686557b79d7b20e99af46eeb226
hariyanijenish@gmail.com <malaviyapunam3@gmail.com>
2024-12-17 11:01:04 +0530

    prectice looping with pattern 11/12/2024

---
+++ js-9/iooping-with-pattern/second-loop.js
@@ -0,0 +1,69 @@
+
+
+
+let i,j ;
+
+for(i=1 ; i<=5 ; i++){
+     data = ""
+    for(j=1 ;j<=i ; j++){
+        data += i + " ";
+    }
+    console.log(data," ");
+}
+{
+let i,j ;
+
+for(i=1 ; i<=5 ; i++){
+     data = ""
+     for(j=1 ;j<=i ; j++){
+        space = ""
+        data += i + " ";
+        space += j + " ";
+    }   
+    console.log(space);
+    
+    console.log(data," ");
+}
+}
+
+
+
+
+// {
+// let i,j, k = 1;
+
+// for(i=4 ; i>=1 ; i--){
+//      let data = ""
+//     for(j=i; j>=1 ; j--){
+//         data += j + " ";
+//     }
+//     console.log(data ," ");
+// }
+
+// }
+// {
+//     let i,j ;
+
+// for(i=1 ; i<=5 ; i++){
+//      data = ""
+//     for(j=1 ;j<=i ; j++){
+//         data += i + " ";
+//     }
+//     console.log(data," ");
+// }
+// }
+
+
+
+// {
+// let i,j, k = 1;
+
+// for(i=5 ; i>=1 ; i--){
+//      data = ""
+//     for(j=i; j>=1 ; j--){
+//         data += j + " ";
+//     }
+//     console.log(data ," ");
+// }
+
+// }
