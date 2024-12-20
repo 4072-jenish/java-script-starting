0b61b34a3f336869e86aa2502afe8b989b03dd4d
hariyanijenish@gmail.com <malaviyapunam3@gmail.com>
2024-12-20 10:23:25 +0530

    function prectice 20/12/2024

---
+++ js-9/functions/function.js
@@ -0,0 +1,64 @@
+
+
+function addition(){
+    let a= +prompt ("Enter the number :");
+    let b= +prompt("Enter the number :");
+    let total;
+    total = a + b ;
+    console.log("Addition is : ",total);
+}
+
+function substraction(){
+    let a= +prompt ("Enter the number :");
+    let b= +prompt("Enter the number :");
+    let total;
+    total = a - b ;
+    console.log("Substraction is : ", total);
+}
+
+function multiplication(){
+    let a= +prompt ("Enter the number :");
+    let b= +prompt("Enter the number :");
+    let total;
+    total = a * b ;
+    console.log("Multiplication is : ",total);
+    
+}
+
+function division(){
+    let a= +prompt ("Enter the number :");
+    let b= +prompt("Enter the number :");
+    let total;
+    total = a / b ;
+    console.log("Division is  : ",total);
+}
+
+
+console.log("Press 1 for addition .......");
+console.log("Press 2 for substraction .......");
+console.log("Press 3 for multiplication .......");
+console.log("Press 4 for division .......");
+console.log("Press 0 for exite progrgam .......");
+
+let choise = +prompt("Enter your choise ");
+switch (choise) {
+    case 1:
+        addition();
+        break;
+    case 2:
+        substraction();
+        break;
+    case 3:
+        multiplication();
+        break;
+    case 4:
+        division();
+        break;
+    case 0:
+        console.log("thank you ....");
+        break;
+
+    default:
+        break;
+}
+
