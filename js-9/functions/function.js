ff300178af86fb4e6935624c870c916b91f1fe55
hariyanijenish@gmail.com <malaviyapunam3@gmail.com>
2024-12-24 09:50:23 +0530

    fibonaci series  24/12/2024

--- js-9/functions/function.js
+++ js-9/functions/function.js
@@ -62,3 +62,4 @@ switch (choise) {
         break;
 }
 
+    
---
+++ js-9/recuersion/fibonakit-series-recuersion.js
@@ -0,0 +1,10 @@
+
+
+console.log("Print the fukin fibonaki series : ");
+
+let n = +prompt("Enter number you want to print the fibonaki series ; ");
+
+function fibo(){
+    
+}
+
