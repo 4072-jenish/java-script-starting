2c711a448b87c70880406786f26a4e816ccf8268
hariyanijenish@gmail.com <malaviyapunam3@gmail.com>
2025-01-21 10:28:20 +0530

    oop update 21/01/2025

---
+++ OOPS/inharitance/inharitance.js
@@ -0,0 +1,25 @@
+
+
+class grandFather{
+    jamin1(){
+        console.log("5 vigha ....");
+    }
+}
+class father extends grandFather{
+    jamin2(){
+        console.log("3 vigha ....");  
+    }
+}
+class me extends father{
+      
+    jamin3(){
+        this.jamin1();
+        this.jamin2();
+        console.log("10 vigho ....");  
+    }
+}
+
+
+let finalproperties = new me();
+
+finalproperties.jamin3();
